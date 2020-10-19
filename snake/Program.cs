2cf501a5fd4c85560fb26e04ce3ac38083813aa2
ualexeyv <ualexeyv@gmail.com>
2020-10-19 23:53:21 +0500

    create function draw

--- snake/Program.cs
+++ snake/Program.cs
@@ -6,9 +6,17 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+            draw(x1, y1, sym1);
+
             Console.ReadLine();
+        }
+        static void draw (int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
