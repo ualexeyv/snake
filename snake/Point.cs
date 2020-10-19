81a27768325756c7710839facfbdb76d2b032915
ualexeyv <ualexeyv@gmail.com>
2020-10-20 00:26:09 +0500

    create class

---
+++ snake/Point.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace snake
+{
+    public class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
--- snake/Program.cs
+++ snake/Program.cs
@@ -6,7 +6,21 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 2;
+            p1.sym = '#';
+            p1.Draw();
+
+            Point p2 = new Point();
+            p2.x = 5;
+            p2.y = 10;
+            p2.sym = '&';
+            p2.Draw();
+
+            Console.ReadLine();
+
+            /*int x1 = 1;
             int y1 = 3;
             char sym1 = '*';
             draw(x1, y1, sym1);
@@ -16,7 +30,7 @@ namespace snake
         static void draw (int x, int y, char sym)
         {
             Console.SetCursorPosition(x, y);
-            Console.Write(sym);
+            Console.Write(sym);*/
         }
     }
 }
